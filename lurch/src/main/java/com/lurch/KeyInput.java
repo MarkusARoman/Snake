d2e4a79c1006d8a6eb3dfdefd50370fcf08f9056
Markus <markusroman08@gmail.com>
2025-06-13 02:33:21 -0400

    Update KeyInput.java

--- lurch/src/main/java/com/lurch/KeyInput.java
+++ lurch/src/main/java/com/lurch/KeyInput.java
@@ -5,37 +5,47 @@ import org.lwjgl.glfw.GLFW;
 import java.util.HashSet;
 import java.util.Set;
 
-public class KeyInput 
-{
+public class KeyInput {
     private final long windowHandle;
     private final Set<Integer> pressedKeys = new HashSet<>();
     private final Set<Integer> justPressedKeys = new HashSet<>();
     private final Set<Integer> justReleasedKeys = new HashSet<>();
 
-    public KeyInput(long windowHandle) 
-    {
+    public KeyInput(long windowHandle) {
         this.windowHandle = windowHandle;
     }
 
-    public void update() 
-    {
+    private static final int[] RELEVANT_KEYS = {
+        GLFW.GLFW_KEY_UP,
+        GLFW.GLFW_KEY_DOWN,
+        GLFW.GLFW_KEY_LEFT,
+        GLFW.GLFW_KEY_RIGHT,
+        GLFW.GLFW_KEY_SPACE,
+        GLFW.GLFW_KEY_ESCAPE
+    };
+
+    public void update() {
         justPressedKeys.clear();
         justReleasedKeys.clear();
 
-        for (int key = GLFW.GLFW_KEY_SPACE; key <= GLFW.GLFW_KEY_LAST; key++) 
+        for (int key : RELEVANT_KEYS) 
         {
             boolean isDown = GLFW.glfwGetKey(windowHandle, key) == GLFW.GLFW_PRESS;
             boolean wasDown = pressedKeys.contains(key);
 
-            if (isDown && !wasDown) {
+            if (isDown && !wasDown) 
+            {
                 justPressedKeys.add(key);
                 pressedKeys.add(key);
-            } else if (!isDown && wasDown) {
+            } 
+            else if (!isDown && wasDown) 
+            {
                 justReleasedKeys.add(key);
                 pressedKeys.remove(key);
             }
         }
     }
+
 
     public boolean isKeyDown(int key) 
     {
@@ -43,12 +53,13 @@ public class KeyInput
     }
 
     public boolean isKeyPressed(int key) 
-{
+    {
         return justPressedKeys.contains(key);
     }
 
     public boolean isKeyReleased(int key) 
-{
+    {
         return justReleasedKeys.contains(key);
     }
 }
+
